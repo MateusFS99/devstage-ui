b07a69f1349d0bf9ad18736bf9c274e476086cca
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2025-02-20 01:09:52 -0300

    Feat: Add UI components and styles for buttons and inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import Button from "@/components/button";
+import IconButton from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        Enviar <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <InputRoot>
+        <InputIcon>
+          <Mail className="size-5" />
+        </InputIcon>
+
+        <InputField placeholder="Digite seu e-mail" />
+      </InputRoot>
+
+      <InputRoot error>
+        <InputIcon>
+          <Mail className="size-5" />
+        </InputIcon>
+
+        <InputField placeholder="Digite seu e-mail" />
+      </InputRoot>
     </main>
   );
 }
