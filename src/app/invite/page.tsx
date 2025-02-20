12dc460ea3d5cf3040f2521e5506db3daa2740df
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2025-02-20 20:28:19 -0300

    Feat: Implement invite link input, subscription form, and ranking display components

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,15 +1,11 @@
 import Image from 'next/image'
 
-import IconButton from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
-
 import logo from '../../../assets/logo.svg'
-import medalCooper from '../../../assets/medal-cooper.svg'
-import medalGold from '../../../assets/medal-gold.svg'
-import medalSilver from '../../../assets/medal-silver.svg'
+import { InviteLinkInput } from './invite-link-input'
+import { Ranking } from './ranking'
+import { Stats } from './stats'
 
-export default function Invite() {
+export default function InvitePage() {
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -36,110 +32,13 @@ export default function Invite() {
             </p>
           </div>
 
-          <InputRoot>
-            <InputIcon>
-              <Link className="size-5" />
-            </InputIcon>
+          <InviteLinkInput />
 
-            <InputField
-              defaultValue="http://devstage.com/subscription/561651616515"
-              readOnly
-            />
-
-            <IconButton className="-mr-2">
-              <Copy className="size-5" />
-            </IconButton>
-          </InputRoot>
-
-          <div className="grid gap-3 md:grid-cols-3">
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Acessos ao link
-              </span>
-              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Inscrições feitas
-              </span>
-              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3º
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Posição no ranking
-              </span>
-              <Medal className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-          </div>
+          <Stats />
         </div>
       </div>
 
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Ranking de indicações
-        </h2>
-
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Victor Gabriel
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-
-            <Image
-              src={medalGold}
-              alt="gold_medal"
-              className="absolute top-0 right-8"
-            />
-          </div>
-
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2º</span> | Arthur Fakouri
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1020
-            </span>
-
-            <Image
-              src={medalSilver}
-              alt="gold_medal"
-              className="absolute top-0 right-8"
-            />
-          </div>
-
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3º</span> | Mateus Fonseca
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1010
-            </span>
-
-            <Image
-              src={medalCooper}
-              alt="gold_medal"
-              className="absolute top-0 right-8"
-            />
-          </div>
-        </div>
-      </div>
+      <Ranking />
     </div>
   )
 }
---
+++ src/app/invite/ranking.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image'
+
+import medalCooper from '../../../assets/medal-cooper.svg'
+import medalGold from '../../../assets/medal-gold.svg'
+import medalSilver from '../../../assets/medal-silver.svg'
+
+export function Ranking() {
+  return (
+    <div className="w-full max-w-[440px] space-y-5">
+      <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+        Ranking de indicações
+      </h2>
+
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">1º</span> | Victor Gabriel
+          </span>
+
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={medalGold}
+            alt="gold_medal"
+            className="absolute top-0 right-8"
+          />
+        </div>
+
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">2º</span> | Arthur Fakouri
+          </span>
+
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1020
+          </span>
+
+          <Image
+            src={medalSilver}
+            alt="gold_medal"
+            className="absolute top-0 right-8"
+          />
+        </div>
+
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">3º</span> | Mateus Fonseca
+          </span>
+
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1010
+          </span>
+
+          <Image
+            src={medalCooper}
+            alt="gold_medal"
+            className="absolute top-0 right-8"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
