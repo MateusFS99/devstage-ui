b4290a716fbb110437979df81f7b20bfd06270ea
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2025-02-20 10:58:15 -0300

    Feat: Remove unused UI components and add logo SVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,7 @@
-import Button from '@/components/button'
-import IconButton from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { ArrowRight, Copy, Mail } from 'lucide-react'
-
 export default function Home() {
   return (
     <main>
-      <Button>
-        Enviar <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <InputRoot>
-        <InputIcon>
-          <Mail className="size-5" />
-        </InputIcon>
-
-        <InputField placeholder="Digite seu e-mail" />
-      </InputRoot>
-
-      <InputRoot error>
-        <InputIcon>
-          <Mail className="size-5" />
-        </InputIcon>
-
-        <InputField placeholder="Digite seu e-mail" />
-      </InputRoot>
+      <h1>Home</h1>
     </main>
   )
 }
