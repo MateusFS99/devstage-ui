31be646232b4933b8ff74ae53095a5094eab6774
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2025-02-20 21:08:04 -0300

    Feat: Pass invite link as a prop to InviteLinkInput component and implement copy to clipboard functionality

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,20 +1,27 @@
+'use client'
+
 import IconButton from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { Copy, Link } from 'lucide-react'
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyToClipboard() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        defaultValue="http://devstage.com/subscription/561651616515"
-        readOnly
-      />
+      <InputField defaultValue={inviteLink} readOnly />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyToClipboard}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
