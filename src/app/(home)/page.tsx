c2c4b810a22b25f92bbec213a8887ca4453a0955
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2025-02-23 01:06:26 -0300

    Feat: Add date-fns dependency and implement utility functions for date formatting and currency display

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,47 +1,88 @@
+'use client'
+
 import Image from 'next/image'
 
+import { type Event, getEventByPrettyName } from '@/http/api'
+import { formatNumberToBrlCurrency } from '@/utils/CurrencyUtils'
+import { concatTime, formatDisplayDate } from '@/utils/DateUtils'
 import { Radio } from 'lucide-react'
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import logo from '../../../assets/logo.svg'
 import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
+  const eventPrettyName = useSearchParams().get('event')
+  const referrer = useSearchParams().get('referrer')
+  const [event, setEvent] = useState<Event>()
+
+  useEffect(() => {
+    getEventByPrettyName(eventPrettyName!).then(res => {
+      setEvent(res)
+    })
+  }, [eventPrettyName])
+
+  function getFirstWord(text: string) {
+    const firstSpaceIndex = text.trim().indexOf(' ')
+
+    return firstSpaceIndex === -1
+      ? text.trim()
+      : text.trim().substring(0, firstSpaceIndex)
+  }
+
+  function getRemainingText(text: string) {
+    const firstSpaceIndex = text.trim().indexOf(' ')
+    return firstSpaceIndex === -1
+      ? ''
+      : text
+          .trim()
+          .substring(firstSpaceIndex + 1)
+          .trim()
+  }
+
   return (
-    <div className="min-h-dvh flex flex-col justify-center gap-16">
-      <div className="flex flex-col gap-8 items-center md:items-start">
-        <Image src={logo} alt="devstage" width={108.5} height={30} />
+    event && (
+      <div className="min-h-dvh flex flex-col justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center md:items-start">
+          <Image src={logo} alt="devstage" width={108.5} height={30} />
 
-        <h1 className="text-4xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
-          <span className="text-blue">CodeCraft</span>
-          Summit 2025
-        </h1>
-      </div>
-
-      <div className="flex gap-5 items-stretch flex-col md:flex-row">
-        <div className="flex-1 bg-gray-700 border-gray-600 rounded-2xl p-8 space-y-6">
-          <div className="flex items-center justify-between">
-            <h2 className="font-heading font-semibold text-gray-200 text-xl">
-              Sobre o evento
-            </h2>
-            <span className="text-purple font-semibold text-xs flex items-center gap-2">
-              <Radio className="size-5" />
-              AO VIVO
-            </span>
-          </div>
-
-          <p className="text-gray-300 leading-relaxed text-sm md:text-base">
-            Um evento feito por e para pessoas desenvolvedoras apaixonadas por
-            criar soluções inovadoras e compartilhar conhecimento. Vamos
-            mergulhar nas tendências mais recentes em desenvolvimento de
-            software, arquitetura de sistemas e tecnologias emergentes, com
-            palestras, workshops e hackathons.
-            <br />
-            <br />
-            Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
-          </p>
+          <h1 className="text-4xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
+            <span className="text-blue">{getFirstWord(event.title!)}</span>
+            {getRemainingText(event.title!)}
+          </h1>
         </div>
 
-        <SubscriptionForm />
+        <div className="flex gap-5 items-stretch flex-col md:flex-row">
+          <div className="flex-1 bg-gray-700 border-gray-600 rounded-2xl p-8 space-y-6">
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading font-semibold text-gray-200 text-xl">
+                Sobre o evento
+              </h2>
+              <span className="text-purple font-semibold text-xs flex items-center gap-2">
+                <Radio className="size-5" />
+                {event.location}
+              </span>
+            </div>
+
+            <p className="text-gray-300 leading-relaxed text-sm md:text-base">
+              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento. Vamos
+              mergulhar nas tendências mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, workshops e hackathons.
+              <br />
+              <br />
+              Datas: {formatDisplayDate(event.startDate!, event.endDate!)}
+              <br />
+              Horário: {concatTime(event.startTime!, event.endTime!)}
+              <br />
+              Preço: {formatNumberToBrlCurrency(event.price!)}
+            </p>
+          </div>
+
+          <SubscriptionForm />
+        </div>
       </div>
-    </div>
+    )
   )
 }
